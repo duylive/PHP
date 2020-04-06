a93fb6607973456e71aa87ec8539ad6685e878d4
duylive <duylive92@gmail.com>
2020-04-06 16:41:19 -0700

    new

---
+++ array_&_function_PHP/find_min_in_array.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php
+$array = [
+    0,11,4,7,9,2,6,8,-34,3,19,5,1,-27,25,37,23,-2,-18,
+];
+
+$min = null;
+$index = null;
+
+for ($i = 0; $i < count($array); $i++) {
+    if ($min == null) {
+        $min = $array[$i];
+        $index = $i;
+    } else {
+        if ($array[$i] < $min) {
+            $min = $array[$i];
+            $index = $i;
+        }
+    }
+}
+
+echo "Giá trị lớn nhất là $min, nằm tại vị trí $index";
+?>
+</body>
+</html>
