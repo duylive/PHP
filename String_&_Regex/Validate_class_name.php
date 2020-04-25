fb604c711e1f175eb081f43c49c8c4fdd78a6de3
duylive <duylive92@gmail.com>
2020-04-24 17:29:29 -0700

    new

---
+++ String_&_Regex/Validate_class_name.php
@@ -0,0 +1,17 @@
+<?php
+
+$class = "/^[^@#$%&*?]*[A]|[C]|[P]+[0-9]+[0-9]+[0-9]+[0-9]+[G-M]$/";
+
+function checkNameClass($str)
+{
+    $class = "/^[^@#$%&*?]*[A]|[C]|[P]+[0-9]+[0-9]+[0-9]+[0-9]+[G-M]$/";
+    if (preg_match($class, $str)) {
+        echo "This class name is invalid";
+    } else {
+        echo "This class name is not invalid";
+    }
+}
+
+checkNameClass("C0220H");
+echo "<br/>";
+checkNameClass("B0220H");
